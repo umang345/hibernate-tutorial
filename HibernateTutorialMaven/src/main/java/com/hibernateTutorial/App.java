bf0916d906b6a81ac70fe7ab31fc9b1de9bf3e96
Umang <umangagarwal345@gmail.com>
2022-01-20 19:17:31 +0530

    Made hibernate seesion factory object

--- HibernateTutorialMaven/src/main/java/com/hibernateTutorial/App.java
+++ HibernateTutorialMaven/src/main/java/com/hibernateTutorial/App.java
@@ -1,5 +1,8 @@
 package com.hibernateTutorial;
 
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
 /**
  * Hello world!
  *
@@ -9,5 +12,12 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        
+        Configuration cfg = new Configuration();
+        cfg.configure("hibernate.cfg.xml");
+        SessionFactory factory = cfg.buildSessionFactory();
+        
+        System.out.println(factory);
+        System.out.println(factory.isClosed());
     }
 }
